b4ae027e0a0e14069e76abe4f6d4e8161a8f894e
it17156134_malkini <it17156134@my.sliit.lk>
2021-02-05 06:18:12 +0530

    initialize front end

---
+++ src/app/school.service.ts
@@ -0,0 +1,35 @@
+// Angular
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// RxJS
+import { Observable } from 'rxjs';
+
+// Models
+import { environment } from '../environments/environment.prod';
+
+const API_ENDPOINT = 'schools';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SchoolService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  /* CREATE =>  POST: send a new school to the server */
+
+  // tslint:disable-next-line:typedef
+  addSchool(school: any) {
+    console.log('school', school);
+    return this.http.post<any>(environment.apiHost + API_ENDPOINT + '/add', school);
+  }
+
+  /* RETRIEVE  =>  GET: get a list of schools */
+  getAllSchools(): Observable<any> {
+    console.log(environment.apiHost + API_ENDPOINT + '/list');
+    return this.http.get<any>(environment.apiHost + API_ENDPOINT + '/list');
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // apiHost : 'http://localhost:3001/',
+  apiHost : 'https://sheltered-beyond-68118.herokuapp.com/'
+};
